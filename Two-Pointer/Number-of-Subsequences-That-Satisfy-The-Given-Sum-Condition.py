e4fa6f679bf4d38b9978b5dce3e51aee432d9ce1
Sathish111j <sathish111j@gmail.com>
2024-03-31 11:13:12 +0530

    solitons added

--- Two-Pointer/Number-of-Subsequences-That-Satisfy-The-Given-Sum-Condition.py
+++ Two-Pointer/Number-of-Subsequences-That-Satisfy-The-Given-Sum-Condition.py
@@ -1,14 +1,14 @@
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
         nums.sort()
-        result = 0
-        modulo = 10**9 + 7
+        result=0
+        modulo=10**9+7
+        right=len(nums)-1
 
         for i, left in enumerate(nums):
-            right = len(nums) - 1  # reset right pointer for each left pointer
-            while left + nums[right] > target and i <= right:
-                right -= 1
-            if i <= right:
-                result += pow(2, right - i, modulo)
-                result %= modulo
+            while (left + nums[right]) > target and i<=right:
+                right-=1
+            if i<=right:
+                result += (2**(right-i)) 
+                result%=modulo
         return result
