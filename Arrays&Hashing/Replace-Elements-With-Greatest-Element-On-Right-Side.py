632863acaa1c37f28eb78345d0907400b712a665
Sathish111j <sathish111j@gmail.com>
2024-05-14 22:35:47 +0530

    solution added

---
+++ Arrays&Hashing/Replace-Elements-With-Greatest-Element-On-Right-Side.py
@@ -0,0 +1,18 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        n=len(arr)
+        if n ==1:
+            return [-1]
+        
+        right_max=arr[-1]
+        arr[-1]=-1
+
+        for i in range (n-2,-1,-1):
+            current=arr[i]
+            arr[i]=right_max
+            if current>right_max:
+                right_max=current
+        return arr
+
+
+        
