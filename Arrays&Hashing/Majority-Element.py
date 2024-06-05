2ccdf6f7295c5db64c4ed4ebe8cbd46acdb2efd2
Sathish111j <sathish111j@gmail.com>
2024-06-05 20:58:29 +0530

    solution added

--- Arrays&Hashing/Majority-Element.py
+++ Arrays&Hashing/Majority-Element.py
@@ -1,16 +1,28 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hashtable = defaultdict(int)
-        for num in nums:
-            if num in nums:
-                hashtable[num]+=1
-        maxvalue=0
-        maxkey=0
-        for key, value in hashtable.items():
-            if value>maxvalue:
-                maxkey=key
-                maxvalue=value
-        return maxkey 
+        # hashtable = defaultdict(int)
+        # for num in nums:
+        #     hashtable[num]+=1
+        # maxvalue=0
+        # maxkey=0
+        # for key, value in hashtable.items():
+        #     if value>maxvalue:
+        #         maxkey=key
+        #         maxvalue=value
+        # return maxkey 
+
+        count=0
+        ans=None
+
+        for num in nums :
+            if count ==0:
+                ans=num
+            if num ==ans :
+                count+=1
+            else :
+                count-=1
+        
+        return ans
 
 
             
