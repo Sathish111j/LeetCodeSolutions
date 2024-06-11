e288031932eb08029e92cee8174dcf424c458f69
Sathish111j <sathish111j@gmail.com>
2024-06-11 13:12:21 +0530

    solution added

---
+++ Stack/rename_stack_files.sh
@@ -0,0 +1,5 @@
+mv "Baseball-Game.py" "682.-Baseball-Game.py"
+mv "Decode-String.py" "394.-Decode-String.py"
+mv "Implement-Stack-Using-Queues.py" "225.-Implement-Stack-Using-Queues.py"
+mv "Remove-All-Adjacent-Duplicates-in-String.py" "1047.-Remove-All-Adjacent-Duplicates-in-String.py"
+mv "Removing-Stars-From-a-String.py" "2390.-Removing-Stars-From-a-String.py"
