4623825a24cf3f8ecaa1782405dfd270670443a1
Sathish111j <sathish111j@gmail.com>
2024-06-19 11:49:14 +0530

    solution added

--- Back-Tracking/46.-Permutations.py
+++ Back-Tracking/46.-Permutations.py
@@ -11,7 +11,8 @@ class Solution:
 
             for perm in perms:
                 perm.append(n)
-
+            # put back the first element at last, so now 2nd element will be first 
+            # and will be considered in the next iteration
             result.extend(perms)
             nums.append(n)
         return result
