98cd2a256d9b82212ae3ae2905c34621b1b1e377
Sathish111j <sathish111j@gmail.com>
2024-06-24 23:23:23 +0530

    solution added

---
+++ Stack/946.-Validate-Stack-Sequences.py
@@ -0,0 +1,10 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        pop_index = 0
+        for element in pushed:
+            stack.append(element) 
+            while stack and stack[-1] == popped[pop_index]:
+                stack.pop()  
+                pop_index += 1 
+        return not stack
