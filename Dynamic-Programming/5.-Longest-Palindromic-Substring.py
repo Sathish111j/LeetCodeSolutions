90462d0bea675b64c74cda13dffb29e1cfcdb821
Sathish111j <sathish111j@gmail.com>
2024-06-27 17:36:35 +0530

    solution  added

---
+++ Dynamic-Programming/5.-Longest-Palindromic-Substring.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        self.res=""
+
+        def helper(l,r):
+            while l>=0 and r <len(s) and s[l]==s[r]:
+                if r-l+1 > len(self.res):
+                    self.res=s[l:r+1]
+                l-=1
+                r+=1
+
+
+        for i in range(len(s)):
+            helper(i,i) # for odd lenght
+            helper(i,i+1) # for even lenght
+
+        return self.res
+
