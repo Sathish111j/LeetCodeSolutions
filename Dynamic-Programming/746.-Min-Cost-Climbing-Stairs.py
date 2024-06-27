74b10e2945476efba3345969dc244ab43536e26a
Sathish111j <sathish111j@gmail.com>
2024-06-27 15:45:21 +0530

    solution  added

--- Dynamic-Programming/746.-Min-Cost-Climbing-Stairs.py
+++ Dynamic-Programming/746.-Min-Cost-Climbing-Stairs.py
@@ -7,4 +7,15 @@ class Solution:
             cost[i]+=min(cost[i+1],cost[i+2])
 
         return min (cost[0],cost[1])
-        
+# class Solution:
+#     def minCostClimbingStairs(self, cost: List[int]) -> int:
+#         dp = [0 for _ in range(len(cost)+1)]
+
+#         #step 0 and 1 both cost 0
+#         for i in range(2, len(cost)+1):
+#             # we will take the lesser total cost between getting 
+#             # here from one step down or two steps down
+#             dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
+
+#         # we care about the cost to get past the last step
+#         return dp[len(cost)]
