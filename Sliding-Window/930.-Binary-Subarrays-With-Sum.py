2fa066f87b7d08b0cf5c47593a0a34707af77107
Sathish111j <sathish111j@gmail.com>
2024-06-30 16:30:30 +0530

    solution

--- Sliding-Window/930.-Binary-Subarrays-With-Sum.py
+++ Sliding-Window/930.-Binary-Subarrays-With-Sum.py
@@ -15,6 +15,6 @@ class Solution:
 
         return atMost(goal) - atMost(goal - 1)
 
-# atMost(goal)` gives  the count of subarrays with sum <= goal
-# atMost(goal - 1)` gives us the count of subarrays with sum < goal
+# atMost(goal) gives  the count of subarrays with sum <= goal
+# atMost(goal - 1) gives us the count of subarrays with sum < goal
 # The difference is exactly the count of subarrays with sum equal to goal
