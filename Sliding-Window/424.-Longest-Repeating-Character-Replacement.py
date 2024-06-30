25bfd63d01604e5e587cef420db645b19b105f53
Sathish111j <sathish111j@gmail.com>
2024-06-30 15:05:54 +0530

    solution added

--- Sliding-Window/424.-Longest-Repeating-Character-Replacement.py
+++ Sliding-Window/424.-Longest-Repeating-Character-Replacement.py
@@ -1,16 +1,16 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count={}
-        res=0
-
-        l=0
-        maxf=0
-        for r in range(len(s)):
-            count[s[r]]=1+count.get(s[r],0)
-            maxf=max(maxf,count[s[r]])
-
-            while(r-l+1)-maxf>k:
-                count[s[l]]-=1
-                l+=1
-            res=max(res,r-l+1)
-        return res
+        char_count = {}
+        max_length = 0
+        max_count = 0
+        start = 0
+        
+        for end in range(len(s)):
+            char_count[s[end]] = char_count.get(s[end], 0) + 1
+            max_count = max(max_count, char_count[s[end]])
+            if (end - start + 1) - max_count > k:
+                char_count[s[start]] -= 1
+                start += 1
+            max_length = max(max_length, end - start + 1)
+        
+        return max_length
