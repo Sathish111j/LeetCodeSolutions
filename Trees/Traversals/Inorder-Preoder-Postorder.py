f0ec4aec17830d8f708b44aeb658b8367730970b
Sathish111j <sathish111j@gmail.com>
2024-07-01 14:40:32 +0530

    traversal added

---
+++ Trees/Traversals/Inorder-Preoder-Postorder.py
@@ -0,0 +1,19 @@
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.value, end=' ')
+        inorder_traversal(root.right)
+
+def preorder_traversal(root):
+    if root:
+        print(root.value, end=' ')
+        preorder_traversal(root.left)
+        preorder_traversal(root.right)
+
+def postorder_traversal(root):
+    if root:
+        postorder_traversal(root.left)
+        postorder_traversal(root.right)
+        print(root.value, end=' ')
+
+
