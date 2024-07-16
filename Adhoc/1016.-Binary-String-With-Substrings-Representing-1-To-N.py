0e4f6ee4ab9e9a27f9684eb7d31f58a86f52b1e1
Sathish111j <sathish111j@gmail.com>
2024-07-16 19:01:56 +0530

    solution added

--- Adhoc/1016.-Binary-String-With-Substrings-Representing-1-To-N.py
+++ Adhoc/1016.-Binary-String-With-Substrings-Representing-1-To-N.py
@@ -1,3 +1,14 @@
 class Solution:
-    def rotateString(self, s: str, goal: str) -> bool:
-        return len(s)==len(goal) and s in goal+ goal
+    def queryString(self, s: str, n: int) -> bool:
+        def DecimalToBinary(num):
+            binary_str = ""
+            while num > 0:
+                binary_str = str(num % 2) + binary_str
+                num //= 2
+            return binary_str
+
+        for i in range(1, n + 1):
+            if DecimalToBinary(i) not in s:
+                return False
+        return True
+         
