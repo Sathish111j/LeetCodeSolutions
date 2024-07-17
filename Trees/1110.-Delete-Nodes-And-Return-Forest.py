8aba2694432387df47f788672b0b7f508e4c7710
Sathish111j <sathish111j@gmail.com>
2024-07-17 19:52:41 +0530

    solution added

---
+++ Trees/1110.-Delete-Nodes-And-Return-Forest.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        toDelete=set(to_delete)
+
+        forest=[]
+
+        def dfs(node,root):
+            if not node:
+                return None
+            
+            isToBeDeleted=node.val in toDelete
+
+            if root and not isToBeDeleted:
+                forest.append(node)
+            
+            node.left=dfs(node.left,isToBeDeleted)
+            node.right=dfs(node.right,isToBeDeleted)
+
+            if isToBeDeleted:
+                return None
+            else:
+                return node
+        dfs(root,True)
+
+        return forest
