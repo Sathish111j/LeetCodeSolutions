e791fef31eb90a752a280ee9ecc56cdef0115069
Sathish111j <sathish111j@gmail.com>
2024-07-23 10:26:03 +0530

    solution added

---
+++ Dynamic-Programming/3.DP-Subsequence/Geeks-For-Geeks/Unbounded-KnapSack.py
@@ -0,0 +1,15 @@
+def unboundedKnapsack(n, W, val, wt):
+    dp = [[0 for _ in range(W + 1)] for _ in range(n)]
+
+    for W in range(W + 1):
+        dp[0][W] = (W // wt[0]) * val[0]
+
+    for ind in range(1, n):
+        for W in range(W + 1):
+            notTake = dp[ind - 1][W]
+            take = 0
+            if wt[ind] <= W:
+                take = val[ind] + dp[ind][W - wt[ind]]
+            dp[ind][W] = max(take, notTake)
+
+    return dp[n - 1][W]
