5220e433636dc1c0f938a862784cb4681dacdb89
Sathish111j <sathish111j@gmail.com>
2025-02-06 14:52:12 +0530

    solution added

---
+++ Arrays&Hashing/1726.-Tuple-with-Same-Product.py
@@ -0,0 +1,14 @@
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        product=defaultdict(int)
+        count=0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                prd=nums[i]*nums[j]
+                count+=product[prd]*8
+                product[prd]+=1
+
+        return count
+                            
+                            
+                            
