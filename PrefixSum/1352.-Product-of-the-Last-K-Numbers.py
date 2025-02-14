a1b56985baff9b8fccb0d344bce54f44ffe31dfa
Sathish111j <sathish111j@gmail.com>
2025-02-14 23:18:04 +0530

    solution added

---
+++ PrefixSum/1352.-Product-of-the-Last-K-Numbers.py
@@ -0,0 +1,28 @@
+import math
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.pre_mul = []
+        self.product = 1
+        
+    def add(self, num: int) -> None:
+        if num is not 0:
+            self.product *= num
+            self.pre_mul.append(self.product)
+        else:
+            self.product = 1
+            self.pre_mul = []
+
+    def getProduct(self, k: int) -> int:
+        if k == len(self.pre_mul):
+            return self.pre_mul[-1]
+        elif k > len(self.pre_mul):
+            return 0
+        else:
+            return int(self.pre_mul[-1]/self.pre_mul[-1-k])
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
